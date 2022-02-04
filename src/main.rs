1a040db66176635e4d57cb0db00d7d1aba95932f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-04 17:22:33 +0530

    Setup basic structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+//  ======
+//  ENTITY
+//  ======
+
+#[derive(Debug)]
+struct RGBColor(u8, u8, u8);
+
+#[derive(Debug)]
+struct Entity {
+    x: i32,
+    speed: i32,
+    color: RGBColor,
+}
+
+//  ======
+//  STREAM
+//  ======
+
+struct Stream {
+    entities: Vec<Entity>,
+}
+
+impl Stream {
+    fn new() -> Self {
+        Stream {
+            entities: Vec::new(),
+        }
+    }
+
+    fn generate_entities(&mut self) {
+        for i in 0..5 {
+            self.entities.push(Entity {
+                x: i,
+                speed: 1,
+                color: RGBColor(100, 255, 100),
+            });
+        }
+    }
+}
+
+//  ====
+//  MAIN
+//  ====
+
 fn main() {
-    println!("Hello, world!");
+    //  Instantiate streams vector
+    let mut streams: Vec<Stream> = Vec::new();
+
+    //  Generate stream entities
+    for _i in 0..10 {
+        let mut stream = Stream::new();
+        stream.generate_entities();
+        streams.push(stream);
+    }
+
+    //  Render streams
+    for stream in streams {
+        println!("{:?}", stream.entities);
+    }
 }

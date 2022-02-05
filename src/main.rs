b20da603383b391906b59d5397b92b07ef4a3764
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-05 19:53:06 +0530

    Adjust to terminal size

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ use utils::RGBColor;
 //  CONFIGURATION
 //  =============
 
-const ROWS: i32 = 30;
-const COLUMNS: i32 = 120;
-const COLUMN_SPACING: i32 = 3;
+const COLUMN_SPACING: u16 = 3;
 
 const STREAM_MINCOUNT: i32 = 5;
 const STREAM_MAXCOUNT: i32 = 10;
@@ -26,18 +24,26 @@ const MODE: utils::Mode = utils::Mode::Binary;
 //  ====
 
 fn main() {
+    //  Get Terminal Window Size
+    let (rows, columns) = crossterm::terminal::size().unwrap_or((40, 120));
+
     //  Instantiate streams vector
     let mut streams: Vec<Stream> = Vec::new();
 
     //  Generate stream entities
-    for c in 0..COLUMNS {
+    for c in 0..columns {
+        //  Space out streams
         if c % COLUMN_SPACING != 0 {
             continue;
         }
+
+        //  Generate a stream
         let mut stream = Stream::new(STREAM_MINCOUNT, STREAM_MAXCOUNT, STREAM_COLOR);
         let random_speed = utils::random_between(5, 20);
         let height_offset = utils::random_between(-50, 1);
-        stream.generate_entities(c, height_offset, random_speed, MODE);
+        stream.generate_entities(c as i32, height_offset, random_speed, MODE);
+
+        //  Add stream to streams collection
         streams.push(stream);
     }
 
@@ -45,7 +51,7 @@ fn main() {
     utils::clear_screen();
     loop {
         for stream in streams.iter_mut() {
-            stream.render(ROWS);
+            stream.render(rows as i32);
         }
         std::thread::sleep(std::time::Duration::from_millis(1000 / FPS));
     }

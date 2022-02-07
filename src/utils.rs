79d32e5b856ea98a8a6d74b46ee121048d168133
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-02-07 17:52:00 +0530

    Add random_between tests

--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,10 @@ pub enum Mode {
 }
 
 /// Generate a random number between min and max
-pub fn random_between(min: i32, max: i32) -> i32 {
+pub fn random_between<T: PartialOrd + rand::distributions::uniform::SampleUniform>(
+    min: T,
+    max: T,
+) -> T {
     rand::thread_rng().gen_range(min..max)
 }
 
@@ -42,4 +45,53 @@ pub fn rgb(s: &char, color: RGBColor) -> String {
         color.2,
         s.to_string()
     )
+}
+
+// #[cfg(tests)]
+mod tests {
+
+    #[test]
+    fn no_random_number_between_0_and_1() {
+        assert_eq!(super::random_between(0, 1), 0);
+    }
+
+    #[test]
+    fn random_number_within_range() {
+        let min = -15;
+        let max = 45;
+        for _ in 0..100 {
+            let x = super::random_between(min, max);
+            if x < min || x >= max {
+                panic!("Random number not between specified range")
+            }
+        }
+    }
+
+    #[test]
+    fn generates_a_new_random_number_everytime() {
+        let first = super::random_between(0, 10);
+        let second = super::random_between(0, 10);
+        let third = super::random_between(0, 10);
+        let all_different = first != second && second != third && third != first;
+        assert_eq!(all_different, true);
+    }
+
+    /// Returns the type of the variable
+    #[allow(dead_code)]
+    fn type_of<T>(_: &T) -> &str {
+        std::any::type_name::<T>()
+    }
+    #[test]
+    fn random_i32s_return_i32() {
+        let number = super::random_between(-15, 32);
+        let type_of_number = type_of(&number);
+        assert_eq!(type_of_number, "i32");
+    }
+
+    #[test]
+    fn random_u16s_return_u16() {
+        let number = super::random_between::<u16>(0, 120);
+        let type_of_number = type_of(&number);
+        assert_eq!(type_of_number, "u16");
+    }
 }

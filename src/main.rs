8b357a25748b6714f60ed7d1cdcb7c185eddd8c8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-03-17 12:24:47 +0530

    Run in Alternate Buffer Fixes #24

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod stream;
 mod streams;
 mod utils;
 
+use std::time::Duration;
+
 use streams::Streams;
 
-use crossterm::terminal;
+use crossterm::{event, terminal};
 
 //  ====
 //  MAIN
@@ -22,13 +24,36 @@ fn main() {
         config::STREAM_MIN_COUNT,
         config::STREAM_MAX_COUNT,
         config::MODE,
-        config::STREAM_COLOR
+        config::STREAM_COLOR,
     );
 
+    // Switch to the alternate screen buffer
+    terminal::enable_raw_mode().unwrap();
+
     //  Render the Matrix-Rain on screen
-    utils::clear_screen(); //  Clear the screen
+    utils::clear_screen();
     loop {
-        streams.render(rows); //  Render each stream
-        std::thread::sleep(std::time::Duration::from_millis(1000 / config::FPS));   //  Sleep for 1/FPS seconds
+        // Check if 'q' or Ctrl+C has been pressed
+        if event::poll(Duration::from_millis(0)).unwrap() {
+            if let event::Event::Key(event) = event::read().unwrap() {
+                if event.code == event::KeyCode::Char('q')
+                    || (event.modifiers == event::KeyModifiers::CONTROL
+                        && event.code == event::KeyCode::Char('c'))
+                {
+                    break;
+                }
+            }
+        }
+
+        //  Render each stream
+        streams.render(rows);
+
+        //  Sleep for 1/FPS seconds
+        std::thread::sleep(Duration::from_millis(1000 / config::FPS));
     }
+
+    //  Clear screen and disable raw mode before exiting
+    utils::clear_screen();
+    utils::cursor_move_to(0, 0);
+    terminal::disable_raw_mode().unwrap();
 }

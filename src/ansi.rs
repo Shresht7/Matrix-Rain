322d8d4a69bf485948f989f7d8579b29496b403d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-03-18 18:16:46 +0530

    Accept Named and Hex Colors. Fixes #29

--- src/ansi.rs
+++ src/ansi.rs
@@ -40,15 +40,23 @@ impl FromStr for RGBColor {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let parts: Vec<&str> = s.split(',').collect();
 
+        if parts.len() == 1 {
+            if parts[0].starts_with('#') {
+                return parse_hex_color(parts[0]);
+            } else {
+                return parse_named_color(parts[0]);
+            }
+        }
+
         if parts.len() != 3 {
             return Err(anyhow::Error::msg("RGBColor must be in the format 'r,g,b'"));
         }
 
-        Ok(RGBColor(
+        return Ok(RGBColor(
             parts[0].parse()?,
             parts[1].parse()?,
             parts[2].parse()?,
-        ))
+        ));
     }
 }
 
@@ -61,4 +69,29 @@ pub fn rgb(s: &char, color: RGBColor) -> String {
         color.2,
         s.to_string()
     )
+}
+
+// HELPER FUNCTIONS
+// ----------------
+
+pub fn parse_hex_color(color: &str) -> Result<RGBColor, anyhow::Error> {
+    let color = color.trim_start_matches('#');
+    let r = u8::from_str_radix(&color[0..2], 16)?;
+    let g = u8::from_str_radix(&color[2..4], 16)?;
+    let b = u8::from_str_radix(&color[4..6], 16)?;
+    Ok(RGBColor(r, g, b))
+}
+
+pub fn parse_named_color(color: &str) -> Result<RGBColor, anyhow::Error> {
+    match color.to_lowercase().as_str() {
+        "black" => Ok(RGBColor(0, 0, 0)),
+        "red" => Ok(RGBColor(255, 0, 0)),
+        "green" => Ok(RGBColor(0, 255, 0)),
+        "yellow" => Ok(RGBColor(255, 255, 0)),
+        "blue" => Ok(RGBColor(0, 0, 255)),
+        "magenta" => Ok(RGBColor(255, 0, 255)),
+        "cyan" => Ok(RGBColor(0, 255, 255)),
+        "white" => Ok(RGBColor(255, 255, 255)),
+        _ => Err(anyhow::Error::msg("Invalid color name")),
+    }
 }

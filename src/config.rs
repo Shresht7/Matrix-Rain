1aba44f0b496b5ce5565e4aa323a48cc3662e6e0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-03-18 15:03:57 +0530

    Reorganize configuration parameters

--- src/config.rs
+++ src/config.rs
@@ -10,29 +10,31 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = "None")]
 pub struct Config {
-    /// Color of the leading entity in a stream
-    #[clap(long, default_value = "200,255,200")]
-    pub leading_entity_color: ansi::RGBColor,
-
-    /// Minimum and Maximum number of entities per stream
-    #[clap(long, default_value = "5")]
-    pub stream_min_count: u16,
-    #[clap(long, default_value = "25")]
-    pub stream_max_count: u16,
+    /// Character Symbol Set
+    #[clap(long, default_value = "Original")]
+    pub mode: entity::Mode,
 
     /// Default color of the streaming entities
     #[clap(long, default_value = "0,255,70")]
     pub stream_color: ansi::RGBColor,
 
-    /// Stream Spacing
-    #[clap(long, default_value = "2")]
-    pub stream_spacing: u16,
+    /// Color of the leading entity in a stream
+    #[clap(long, default_value = "200,255,200")]
+    pub leading_entity_color: ansi::RGBColor,
 
     /// Frame-Rate
     #[clap(long, default_value = "60")]
     pub fps: u64,
 
-    /// Character Symbol Set
-    #[clap(long, default_value = "Original")]
-    pub mode: entity::Mode,
+    /// Minimum number of entities per stream
+    #[clap(long, default_value = "5")]
+    pub stream_min_count: u16,
+
+    /// Maximum number of entities per stream
+    #[clap(long, default_value = "25")]
+    pub stream_max_count: u16,
+
+    /// Stream Spacing
+    #[clap(long, default_value = "2")]
+    pub stream_spacing: u16,
 }
